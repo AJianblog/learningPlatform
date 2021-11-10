d1d353a3ab34d8b5259db766aa6eae005b72a81f
何志坚 <1305696249@qq.com>
2021-11-10 22:46:01 +0800

    雪花算法生成id

---
+++ src/main/java/com/hzj/learningplatform/common/entity/SnowFlake.java
@@ -0,0 +1,97 @@
+package com.hzj.learningplatform.common.entity;
+
+import com.hzj.learningplatform.common.exceptionHandle.exception.BaseException;
+
+/**
+ * 0 - 0001100101 0001010111 1101000100 1010111000 0 - 10001 - 11001 - 000000000000
+ * 第一个部分，是 1 个 bit：0，这个是无意义的。
+ * 第二个部分是 41 个 bit：表示的是时间戳。
+ * 第三个部分是 5 个 bit：表示的是机房 id，10001。
+ * 第四个部分是 5 个 bit：表示的是机器 id，11001。
+ * 第五个部分是 12 个 bit：表示的序号，就是某个机房某台机器上这一毫秒内同时生成的 id 的序号，000000000000。
+ */
+public class SnowFlake {
+    /**
+     * 起始的时间戳
+     */
+    private static final long START_STMP = 1480166465631L;
+
+    /**
+     * 每一部分占用的位数
+     */
+    private static final long SEQUENCE_BIT = 12; //序列号占用的位数
+    private static final long MACHINE_BIT = 5;   //机器标识占用的位数
+    private static final long DATACENTER_BIT = 5;//数据中心占用的位数
+
+    /**
+     * 每一部分的最大值
+     */
+    private static final long MAX_DATACENTER_NUM = ~(-1L << DATACENTER_BIT);
+    private static final long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
+    private static final long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
+
+    /**
+     * 每一部分向左的位移
+     */
+    private static final long MACHINE_LEFT = SEQUENCE_BIT;
+    private static final long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private static final long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    private final long datacenterId;  //数据中心
+    private final long machineId;     //机器标识
+    private long sequence = 0L; //序列号
+    private long lastStmp = -1L;//上一次时间戳
+
+    public SnowFlake(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * 产生下一个ID
+     *
+     */
+    public synchronized long nextId() {
+        long currStmp = getNewstmp();
+        if (currStmp < lastStmp) {
+            throw new BaseException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStmp == lastStmp) {
+            //相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            //同一毫秒的序列数已经达到最大
+            if (sequence == 0L) {
+                currStmp = getNextMill();
+            }
+        } else {
+            //不同毫秒内，序列号置为0
+            sequence = 0L;
+        }
+
+        lastStmp = currStmp;
+
+        return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
+                | datacenterId << DATACENTER_LEFT       //数据中心部分
+                | machineId << MACHINE_LEFT             //机器标识部分
+                | sequence;                             //序列号部分
+    }
+
+    private long getNextMill() {
+        long mill = getNewstmp();
+        while (mill <= lastStmp) {
+            mill = getNewstmp();
+        }
+        return mill;
+    }
+
+    private long getNewstmp() {
+        return System.currentTimeMillis();
+    }
+}

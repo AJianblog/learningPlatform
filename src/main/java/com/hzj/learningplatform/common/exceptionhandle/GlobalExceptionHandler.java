0e17ea6519135495c65eb89d81b70348e24defc0
何志坚 <1305696249@qq.com>
2021-11-10 22:53:04 +0800

    定义全局异常处理类

---
+++ src/main/java/com/hzj/learningplatform/common/exceptionhandle/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.hzj.learningplatform.common.exceptionhandle;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
